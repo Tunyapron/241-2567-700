5131042078d0e4fab3e7159f5300f7e1319f1846
Tunyapron <tunyapron.k@ku.th>
2025-03-09 12:52:42 +0700

    Error

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
-function submitData () {
+const validateData = (userData) => {
+    let errors = []
+
+    if (!userData.firstname) {
+        errors.push('กรุณากรอกชื่อ')
+    }
+    
+    if (!userData.lastname) {
+        errors.push('กรุณากรอกนามสกุล')
+    }
+    
+    if (!userData.age) {
+        errors.push('กรุณากรอกอายุ')
+    }
+    
+    if (!userData.gender) {
+        errors.push('กรุณากรอกเพศ')
+    }
+    
+    if (!userData.interests) {
+        errors.push('กรุณากรอกความสนใจ')
+    }
+    
+    if (!userData.description) {
+        errors.push('กรุณากรอกคำอธิบาย')
+    }
+    return errors
+}
+const submitData = async () => {
     let firstnameDOM = document.querySelector('input[name=firstname]');
     let lastnameDOM = document.querySelector('input[name=lastname]');
     let ageDOM = document.querySelector('input[name=age]');
-    let genderDOM = document.querySelector('input[name=gender]');
-    let interestDOM = document.querySelectorAll('input[name=interest]:checked');
+    let genderDOM = document.querySelector('input[name=gender]:checked') || {}
+    let interestDOM = document.querySelectorAll('input[name=interest]:checked') || {}
     let descriptionDOM = document.querySelector('textarea[name=description]');
+
+    let messageDOM = document.getElementById('message');
+
+    try {
 
     let interest = '';
     for (let i = 0; i < interestDOM.length; i++) {
@@ -19,9 +51,48 @@ function submitData () {
         firstname: firstnameDOM.value,
         lastname: lastnameDOM.value,
         age: ageDOM.value,
-        genderDOM: genderDOM.value,
-        descriptionDOM: descriptionDOM.value,
-        interest: interest
+        gender: genderDOM.value,
+        description: descriptionDOM.value,
+        interests: interest
     }
+
     console.log('submitData',userData);
+
+        // const errors = validateData(userData)
+
+        // if (errors.length > 0) {
+        //     // error
+        //     throw {
+        //         message:'กรุณากรอกข้อมูลให้ครบถ้วน',
+        //         errors: errors
+        //     }
+        // }
+
+        const response = await axios.post('http://localhost:8000/users', userData)
+        console.log('response',response.data);
+        messageDOM.innerText = 'บันทึกข้อมูลเรียบร้อย'
+        messageDOM.className = 'message success'
+
+    } catch (error) {
+        console.log('error message', error.message);
+        console.log('error', error.errors);
+
+        if (error.response) {
+            console.log('error.response', error.response.data.message);
+            error.message = error.response.data.message
+            error.errors = error.response.data.errors
+        }
+
+        let htmlData = '<div>'
+        htmlData += `<div> ${error.message} </div>`
+        htmlData += '<ul>'
+        for (let i = 0; i < error.errors.length; i++) {
+            htmlData += `<li> ${error.errors[i]} </li>`
+        }
+        htmlData += '</ul>'
+        htmlData += '</div>'
+
+        messageDOM.innerHTML = htmlData
+        messageDOM.className = 'message danger'
+    }
 }
--- server/index.js
+++ server/index.js
@@ -21,6 +21,35 @@ const initMySQL = async () => {
   })
 }
 
+const validateData = (userData) => {
+  let errors = []
+
+  if (!userData.firstname) {
+      errors.push('กรุณากรอกชื่อ')
+  }
+  
+  if (!userData.lastname) {
+      errors.push('กรุณากรอกนามสกุล')
+  }
+  
+  if (!userData.age) {
+      errors.push('กรุณากรอกอายุ')
+  }
+  
+  if (!userData.gender) {
+      errors.push('กรุณากรอกเพศ')
+  }
+  
+  if (!userData.interests) {
+      errors.push('กรุณากรอกความสนใจ')
+  }
+  
+  if (!userData.description) {
+      errors.push('กรุณากรอกคำอธิบาย')
+  }
+  return errors
+}
+
 // app.get('/testdb', (req, res) => {
 //   mysql.createConnection({
 //     host: 'localhost',
@@ -75,16 +104,26 @@ app.post('/users', async (req, res) => {
 
   try {
     let user = req.body;
+    const errors = validateData(user)
+    if (errors.length > 0) {
+      throw { 
+        message: 'กรุณากรอกข้อมูลให้ครบถ้วน', 
+        errors: errors 
+      }
+    }
+
     const results = await conn.query('INSERT INTO users SET ?', user)
     res.json({
       message: 'Create user successfully',
       data: results[0]
    })
   } catch (error) {
-    console.log('error', error.message)  
+    const errorMessage = error.message || 'something went wrong'
+    const errors = error.errors || []
+    console.log('error message:', error.message)
     res.status(500).json({
-      message: 'something went wrong',
-      errorMessage: error.message  
+      message: errorMessage,
+      errors:errors
      })
   }
 })
